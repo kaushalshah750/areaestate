8910508de615f4f9dfe973c26a7a90acbd32d5f9
kaushalshah750 <kaushalshah750@gmail.com>
2025-02-23 02:53:08 +0530

    updaed to angular 18

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,28 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './admin/auth/login/login.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AddEmployeeComponent } from './admin/employee/add-employee/add-employee.component';
 import { SidebarComponent } from './admin/shared/sidebar/sidebar.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { EmployeeComponent } from './admin/employee/employee.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    AddEmployeeComponent,
-    SidebarComponent,
-    EmployeeComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LoginComponent,
+        AddEmployeeComponent,
+        SidebarComponent,
+        EmployeeComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
